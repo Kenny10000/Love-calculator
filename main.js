7d4802dff128d344bc9591eab6a0e5d435265846
Kenny <bolajikehinde2468@gmail.com>
2024-01-25 14:42:06 +0100

    update

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-function calculate(){
+function Calculate(){
 if (playerName.value == "" || loveName.value == ""){
 err.innerHTML = "All input filed are required" 
 err.style.display = "block"
-    // setTimeout(() =>{
-    //     err.style.display = "none"
-    // },5000
-    // )
+    setTimeout(() =>{
+        err.style.display = "none"
+    },5000
+    )
 }
 }
