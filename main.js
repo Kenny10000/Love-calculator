9f9a2db6e678d678994c2d757771544599ba29d4
Kenny <bolajikehinde2468@gmail.com>
2024-01-25 14:24:22 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function calculate(){
+if (playerName.value == "" || loveName.value == ""){
+err.innerHTML = "All input filed are required" 
+err.style.display = "block"
+    // setTimeout(() =>{
+    //     err.style.display = "none"
+    // },5000
+    // )
+}
+}
