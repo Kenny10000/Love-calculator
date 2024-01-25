d3a20dfd3eb18694aad0de94752ebca62651031f
Kenny <bolajikehinde2468@gmail.com>
2024-01-25 20:46:40 +0100

    update

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ err.style.display = "block"
     <h1 class="display-6"> <b style="font-size:1em;">✔✔✔</b>
     <b>
     <p style="color:rgb(4, 254, 25);">You and ${loveName.value} match</p>
-    <b style="font-size:1em;">${randomNumber}%</b>
+    <b style="font-size:1em;color:rgb(4, 254, 25);">${randomNumber}%</b>
     <p style="color:white;">You will not want to lose ${playerName.value}</p>
     </h1>
     `
@@ -25,7 +25,7 @@ err.style.display = "block"
     <h1 class="display-6"> <b style="font-size:1em;">⁉⁉⁉</b>
     <b>
     <p style="color:blue;">You and ${loveName.value} match</p>
-    <b style="font-size:1em;">${randomNumber}%</b>
+    <b style="font-size:1em;color:blue;">${randomNumber}%</b>
     <p style="color:white;"> ${playerName.value}!! dey play oh 😁🙄</p>
     </h1>
     `
@@ -36,7 +36,7 @@ err.style.display = "block"
     <h1 class="display-6"> <b style="font-size:1em;">😆😅😂</b>
     <b>
     <p style="color:yellow;">You and ${loveName.value} match</p>
-    <b style="font-size:1em;">${randomNumber}%</b>
+    <b style="font-size:1em;color:yellow;">${randomNumber}%</b>
     <p style="color:white;"> ${playerName.value} don't trust ${loveName.value}🤣🤪</p>
     </h1>
     `
@@ -48,7 +48,7 @@ else if (randomNumber >= 25) {
     <h1 class="display-6"> <b style="font-size:1em;">😆😅😂</b>
     <b>
     <p style="color:red;">You and ${loveName.value} match</p>
-    <b style="font-size:1em;">${randomNumber}%</b>
+    <b style="font-size:1em;color:red;">${randomNumber}%</b>
     <p style="color:white;"> ${playerName.value} danger zone!!! dey plan your japa</p>
     </h1>
     `
